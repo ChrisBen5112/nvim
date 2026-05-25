c41abbf92c9c010ecc64590b0f4e7bee8946295c
Chris Ben <chrisben5112@proton.me>
2026-05-25 14:07:59 +0530

    lualine added

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    -- enabled = false,
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = {
+            "venv-selector",
+            "encoding",
+            "fileformat",
+            "filetype",
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        options = {
+            theme = 'tokyonight'
+        }
+      }
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  }
